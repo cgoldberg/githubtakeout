0f9133a009d209f93f2c8f4380f6f0a57cb3c567
Corey Goldberg <cgoldberg@gmail.com>
2015-07-08 17:51:44 -0400

    initial import

---
+++ githubtakeout.py
@@ -0,0 +1,31 @@
+from os.path import basename, exists, join
+import shutil
+import tarfile
+
+from git import Repo  # GitPython
+from github import Github  # PyGithub
+
+
+def make_gzip_tarball(source_dir, output_filepath):
+    with tarfile.open(output_filepath, "w:gz") as tar:
+        tar.add(source_dir, arcname=basename(source_dir))
+
+
+def archive_repos(user):
+    github = Github()
+    dir = 'repo_backups'
+    if exists(dir):
+        shutil.rmtree(dir)
+    user = github.get_user(user)
+    for repository in user.get_repos():
+        print repository.full_name
+        repo_path = join(dir, repository.name)
+        Repo.clone_from(repository.git_url, repo_path)
+        tarball_path = join(dir, '{}.tar.gz'.format(repository.name))
+        make_gzip_tarball(repo_path, tarball_path)
+        if exists(repo_path):
+            shutil.rmtree(repo_path)
+
+
+if __name__ == '__main__':
+    archive_repos('cgoldberg')
