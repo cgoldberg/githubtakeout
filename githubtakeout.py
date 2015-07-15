434a653c6387f374f09e3c1c0f9a4c82dc7ad4d0
Corey Goldberg <cgoldberg@gmail.com>
2015-07-15 10:15:27 -0400

    added ability to clone all users gists

--- githubtakeout.py
+++ githubtakeout.py
@@ -2,8 +2,8 @@ from os.path import basename, exists, join
 import shutil
 import tarfile
 
-from git import Repo  # GitPython
-from github import Github  # PyGithub
+from git import Repo
+from github import Github
 
 
 def make_gzip_tarball(source_dir, output_dir, tarball_filename):
@@ -15,20 +15,41 @@ def make_gzip_tarball(source_dir, output_dir, tarball_filename):
 
 def archive_repos(user):
     github = Github()
-    dir = 'repo_backups'
+    repos_dir = 'repo_backups'
     # clobber existing directory to start fresh
-    if exists(dir):
-        shutil.rmtree(dir)
+    if exists(repos_dir):
+        shutil.rmtree(repos_dir)
     user = github.get_user(user)
     for repository in user.get_repos():
-        print repository.full_name
-        repo_path = join(dir, repository.name)
+        print dir(repository)
+        print(repository.full_name)
+        repo_path = join(repos_dir, repository.name)
         Repo.clone_from(repository.git_url, repo_path)
         tarball_filename = '{}.tar.gz'.format(repository.name)
-        make_gzip_tarball(repo_path, dir, tarball_filename)
-        # delete repo since we have it archived
+        make_gzip_tarball(repo_path, repos_dir, tarball_filename)
+        # delete repo once we have it archived
         if exists(repo_path):
             shutil.rmtree(repo_path)
+
+
+def archive_gists(user):
+    github = Github()
+    gists_dir = 'gist_backups'
+    # clobber existing directory to start fresh
+    if exists(gists_dir):
+        shutil.rmtree(gists_dir)
+    user = github.get_user(user)
+    for gist in user.get_gists():
+        print dir(gist)
+        print(gist.id)
+        gist_path = join(gists_dir, gist.id)
+        Repo.clone_from(gist.git_pull_url, gist_path)
+        tarball_filename = '{}.tar.gz'.format(gist.id)
+        make_gzip_tarball(gist_path, gists_dir, tarball_filename)
+        # delete repo once we have it archived
+        if exists(gist_path):
+            shutil.rmtree(gist_path)
+
 
 
 if __name__ == '__main__':
