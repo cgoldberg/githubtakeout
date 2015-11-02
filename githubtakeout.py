25ef9ee574d75cba8e612c929f99db74c91d4863
Corey Goldberg <cgoldberg@gmail.com>
2015-11-02 13:38:47 -0500

    broken version

--- githubtakeout.py
+++ githubtakeout.py
@@ -14,9 +14,9 @@ def make_gzip_tarball(source_dir, output_dir, tarball_filename):
     return output_path
 
 
-def clone_repo(repo, repo_path):
-    print 'cloning: {}'.format(repo.git_url)
-    Repo.clone_from(repo.git_url, repo_path)
+def clone_repo(repo_url, repo_path):
+    print 'cloning: {}'.format(repo_url)
+    Repo.clone_from(repo_url, repo_path)
 
 
 def get_repos(user_name):
@@ -25,12 +25,12 @@ def get_repos(user_name):
     return user.get_repos()
 
 
-def archive_repo(repo, repos_dir, repo_path):
-    tarball_filename = '{}.tar.gz'.format(repo.name)
+def archive_repo(repo_name, repos_dir, repo_path):
+    tarball_filename = '{}.tar.gz'.format(repo_name)
     print 'creating archive: {}'.format(tarball_filename)
     make_gzip_tarball(repo_path, repos_dir, tarball_filename)
     # delete repo after it's archived
-    print 'deleting repo: {}\n'.format(repo.name)
+    print 'deleting repo: {}\n'.format(repo_name)
     if exists(repo_path):
         shutil.rmtree(repo_path)
 
@@ -40,13 +40,17 @@ def export_repos(user_name, include_forked_repos=False):
     if exists(repos_dir):
         shutil.rmtree(repos_dir)
     for repo in get_repos(user_name):
+        repo_path = join(repos_dir, repo.name)
         if repo.source is None:
-            repo_path = join(repos_dir, repo.name)
-            clone_repo(repo, repo_path)
-            archive_repo(repo, repos_dir, repo_path)
+            clone_repo(repo.git_url, repo_path)
+            archive_repo(repo.name, repos_dir, repo_path)
+        else:
+            if include_forked_repos:
+                clone_repo(repo.git_url, repo_path)
+                archive_repo(repo.name, repos_dir, repo_path)
 
 
-def archive_gists(user_name):
+def export_gists(user_name):
     gists_dir = 'gist_backups'
     # clobber existing gists directory to start fresh
     if exists(gists_dir):
@@ -56,18 +60,21 @@ def archive_gists(user_name):
     for gist in user.get_gists():
         print('cloning: {}'.format(gist.id))
         gist_path = join(gists_dir, gist.id)
-        Repo.clone_from(gist.git_pull_url, gist_path)
-        tarball_filename = '{}.tar.gz'.format(gist.id)
-        print 'creating archive: {}'.format(tarball_filename)
-        make_gzip_tarball(gist_path, gists_dir, tarball_filename)
-        # delete repo after it's archived
-        print 'deleting repo: {}\n'.format(gist.id)
-        if exists(gist_path):
-            shutil.rmtree(gist_path)
+        clone_repo(gist.git_pull_url, gist_path)
+        archive_repo(gist.name, gists_dir, gist_path)
+#
+#def archive_
+#        tarball_filename = '{}.tar.gz'.format(gist.id)
+#        print 'creating archive: {}'.format(tarball_filename)
+#        make_gzip_tarball(gist_path, gists_dir, tarball_filename)
+#        # delete repo after it's archived
+#        print 'deleting repo: {}\n'.format(gist.id)
+#        if exists(gist_path):
+#            shutil.rmtree(gist_path)
 
 
 
 if __name__ == '__main__':
     user = 'cgoldberg'
     export_repos(user)
-    archive_gists(user)
+    export_repos(user)
