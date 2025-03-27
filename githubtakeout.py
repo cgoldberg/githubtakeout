3b5df646af82a231738772c32de0485c2a5ecef2
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-03-27 00:44:56 -0400

    Support for authentication for private repos and secret gists

--- githubtakeout.py
+++ githubtakeout.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import getpass
 import logging
 import os
 import shutil
 import tarfile
+import urllib
 import zipfile
 
 from pathlib import Path
@@ -16,6 +18,17 @@ import github
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
+
+
+def add_creds(url, username, token):
+    if token is None:
+        new_url = url
+    else:
+        username = urllib.parse.quote(username)
+        url_parts = urllib.parse.urlparse(url)
+        netloc = f'{username}:{token}@{url_parts.netloc}'
+        new_url = urllib.parse.urlunparse(url_parts._replace(netloc=netloc))
+    return new_url
 
 
 def archive(local_repo_dir, archive_format='zip', is_gist=False):
@@ -46,7 +59,8 @@ def clone_and_archive_repo(repo_url, local_repo_dir, archive_format, include_his
         shutil.rmtree(local_repo_dir)
     except FileNotFoundError:
         pass
-    logger.info(f'cloning repo: {repo_url} to {local_repo_dir}')
+    repo_path = urllib.parse.urlparse(repo_url).path
+    logger.info(f'cloning repo: {repo_path} to {local_repo_dir}')
     start = default_timer()
     try:
         if include_history:
@@ -73,47 +87,87 @@ def clone_and_archive_repo(repo_url, local_repo_dir, archive_format, include_his
         pass
     logger.info('')
 
-def run(username, base_dir, archive_format, include_gists, include_history, list):
+
+def get_user(username, prompt_token):
+    if prompt_token:
+        token = getpass.getpass('Token:')
+        if not token:
+            print(f'Error: auth token cannot be empty')
+            exit(1)
+        auth = github.Auth.Token(token)
+        gh = github.Github(auth=auth)
+    else:
+        token = os.getenv('GITHUB_TOKEN')
+        if token is not None:
+            auth = github.Auth.Token(token)
+            gh = github.Github(auth=auth)
+        else:
+            gh = github.Github()
+    if token is None:
+        try:
+            user = gh.get_user(username)
+        except github.GithubException as e:
+            if e.data['status'] == '404':
+                print(f'Error: user "{username}" not found on GitHub')
+            else:
+                raise e
+            exit(1)
+        repos = user.get_repos()
+    else:
+        # you need to be authenticated and then call the API
+        # with no username to get private repos
+        user = gh.get_user()
+        repos = user.get_repos(affiliation='owner')
+        try:
+            # this just makes an API request so we can exit if unauthorized
+            repos.totalCount
+        except github.GithubException as e:
+            if e.data['status'] == '401':
+                print(f'Error: invalid auth token for user "{username}" on GitHub')
+            else:
+                raise e
+            exit(1)
+    gists = user.get_gists()
+    return user, repos, gists, token
+
+
+def run(username, base_dir, archive_format, include_gists, include_history, list, prompt_token):
     working_dir = os.path.join(base_dir, 'backups')
-    gh = github.Github()
-    try:
-        user = gh.get_user(username)
-    except github.GithubException:
-        print(f'Error: user "{username}" not found on GitHub')
-        exit(1)
-    repos = user.get_repos()
-    num_repos = len([1 for _ in repos])
+    user, repos, gists, token = get_user(username, prompt_token)
+    num_repos = repos.totalCount
     if not list:
         logger.info(f'creating archives in: {working_dir}\n')
     logger.info(f'found {num_repos} repos for user "{username}"\n')
     for repo in repos:
         local_repo_dir = os.path.join(working_dir, repo.name)
+        url = add_creds(repo.clone_url, username, token)
         if list:
-            logger.info(repo.clone_url)
+            logger.info(url)
         else:
             clone_and_archive_repo(
-                repo.clone_url,
+                url,
                 local_repo_dir,
                 archive_format,
                 include_history
             )
     if include_gists:
-        gists = user.get_gists()
-        num_gists = len([1 for _ in gists])
+        num_gists = gists.totalCount
         logger.info('')
         logger.info(f'found {num_gists} gists for user "{username}"\n')
         for gist in gists:
             local_repo_dir = os.path.join(working_dir, gist.id)
+            url = add_creds(gist.git_pull_url, username, token)
             if list:
-                logger.info(gist.git_pull_url)
+                logger.info(url)
             else:
                 clone_and_archive_repo(
-                    gist.git_pull_url,
+                    url,
                     local_repo_dir,
                     archive_format,
                     include_history,
                     is_gist=True
                 )
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -149,6 +203,12 @@ def main():
         action='store_true',
         help='list repos only'
     )
+    parser.add_argument(
+        '--token',
+        default=False,
+        action='store_true',
+        help='prompt for auth token'
+    )
     args = parser.parse_args()
     run(
         username=args.username,
@@ -156,7 +216,8 @@ def main():
         archive_format=args.format,
         include_gists=args.gists,
         include_history=args.history,
-        list=args.list
+        list=args.list,
+        prompt_token=args.token
     )
 
 
