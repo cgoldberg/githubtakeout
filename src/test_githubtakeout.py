7c9febaa3ea34b6bcf4ac8569f4a49f4b045647d
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-11-23 16:28:15 -0500

    Add more tests

--- src/test_githubtakeout.py
+++ src/test_githubtakeout.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 import pytest
@@ -5,10 +6,10 @@ import pytest
 import githubtakeout
 
 
-def test_run_list(tmp_path, caplog):
+def test_run_list_1_match(tmp_path, caplog):
+    caplog.set_level("INFO")
     user = "cgoldberg"
     repo = "githubtakeout"
-    caplog.set_level("INFO")
     githubtakeout.run(
         user,
         tmp_path,
@@ -21,15 +22,53 @@ def test_run_list(tmp_path, caplog):
     )
     assert "creating archives" not in caplog.text
     assert f"found 1 repos for user '{user}'" in caplog.text
+    assert "gists" not in caplog.text
     assert f"{user}/{repo}" in caplog.text
     assert not Path(tmp_path / repo).exists()
 
 
+def test_run_list_0_match(tmp_path, caplog):
+    caplog.set_level("INFO")
+    user = "cgoldberg"
+    githubtakeout.run(
+        user,
+        tmp_path,
+        "this_repo_does_not_exist",
+        "none",
+        False,
+        False,
+        True,
+        False,
+    )
+    assert "creating archives" not in caplog.text
+    assert f"found 0 repos for user '{user}'" in caplog.text
+    assert "gists" not in caplog.text
+
+
+def test_run_list_0_match_with_gists(tmp_path, caplog):
+    caplog.set_level("INFO")
+    user = "cgoldberg"
+    githubtakeout.run(
+        user,
+        tmp_path,
+        "this_repo_does_not_exist",
+        "none",
+        True,
+        False,
+        True,
+        False,
+    )
+    assert "creating archives" not in caplog.text
+    assert f"found 0 repos for user '{user}'" in caplog.text
+    assert f"found 0 repos for user '{user}'" in caplog.text
+
+
 @pytest.mark.parametrize("archive_format", ["zip", "tar"])
 def test_run_archive(archive_format, tmp_path, caplog):
+    caplog.set_level("INFO")
+    backup_dir = "backups"
     user = "cgoldberg"
     repo = "githubtakeout"
-    caplog.set_level("INFO")
     githubtakeout.run(
         user,
         tmp_path,
@@ -40,8 +79,9 @@ def test_run_archive(archive_format, tmp_path, caplog):
         False,
         False,
     )
-    assert "creating archives" in caplog.text
+    assert re.search(f"creating archives in: .*{backup_dir}", caplog.text)
     assert f"found 1 repos for user '{user}'" in caplog.text
+    assert "gists" not in caplog.text
     assert "cloning repo" in caplog.text
     assert f"{user}/{repo}" in caplog.text
     assert "creating archive" in caplog.text
@@ -50,7 +90,35 @@ def test_run_archive(archive_format, tmp_path, caplog):
     assert f"successfully backed up '{repo}' repo" in caplog.text
     assert not Path(tmp_path / repo).exists()
     if archive_format == "tar":
-        extension = ".tar.gz"
+        extension = "tar.gz"
     else:
         extension = archive_format
-    assert Path(tmp_path / "backups" / f"{repo}.{extension}").exists()
+    assert Path(tmp_path / backup_dir / f"{repo}.{extension}").exists()
+
+
+def test_run_no_archive_with_history(tmp_path, caplog):
+    caplog.set_level("INFO")
+    backup_dir = "backups"
+    user = "cgoldberg"
+    repo = "githubtakeout"
+    githubtakeout.run(
+        user,
+        tmp_path,
+        repo,
+        "none",
+        False,
+        False,
+        False,
+        False,
+    )
+    assert re.search(f"creating archives in: .*{backup_dir}", caplog.text)
+    assert f"found 1 repos for user '{user}'" in caplog.text
+    assert "gists" not in caplog.text
+    assert "cloning repo" in caplog.text
+    assert f"{user}/{repo}" in caplog.text
+    assert "creating archive" not in caplog.text
+    assert "archive size" not in caplog.text
+    assert "deleting repo" not in caplog.text
+    assert f"successfully backed up '{repo}' repo" in caplog.text
+    assert Path(tmp_path / repo).exists()
+    assert Path(tmp_path / repo / ".git").exists()
